2ec5cb4f2b34192f75088262294064f55a340858
Ruveyda <ruvis.kus@gmail.com>
2018-03-23 23:33:18 +0300

    Update Hw4-1.py

--- Hw4-1.py
+++ Hw4-1.py
@@ -63,14 +63,6 @@ imshow(style_image)
 # In[31]:
 
 def gram_matrix(A):
-    """
-    Argument:
-    A -- matrix of shape (n_C, n_H*n_W)
-
-    Returns:
-    GA -- Gram matrix of A, of shape (n_C, n_C)
-    """
-
 
     GA = tf.matmul(A, tf.transpose(A))
 
